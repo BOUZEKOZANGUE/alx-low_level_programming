958a83402a8e2bd28188220e2806e1dd7cdae2a3
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-06-30 17:31:52 -0700

    Task 4 Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *the use function puts 
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	puts("\n""Programming is like building a multilingual puzzle")
+
+	    return (0);
+
+}

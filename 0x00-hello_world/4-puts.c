4cd408bce54b597783c0a01caf660abe10d2e70a
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-06-30 18:28:59 -0700

    Task 4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - Entry poin 
  * Return: Always 0 (Success)
  **/
-int main(void)
+	int main(void)
 
-{
+	{	
 	puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
 
-}
+	}	

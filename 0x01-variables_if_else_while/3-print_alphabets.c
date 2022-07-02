ab122d5bab146e92d9b4219cdd711e30879f4e54
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-07-01 18:04:02 -0700

    Task 3alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,12 @@ int main(void)
 {
 	char c='a';
 
-	while (c <= 'z');
+	while (c <= 'z'; c <='Z');
 	{
 		putchar(c);
 		c++;
 	}
-	while (c <= 'Z');
-	{
-		putchar(c);
-		c++;
-	}
-
+	
 	putchar('\n');
 	return (0);
 }

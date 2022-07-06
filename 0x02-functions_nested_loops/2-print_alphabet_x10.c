216f9a63c08660c3069b286ac5f39d89ac696dd3
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-07-06 06:20:51 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-initialisation
+ * only use the putchar twice in my code
+ * Return :Always(0)
+ */
+void print_alphabet_x10(void)
+{
+	char car;
+	int i = 0;
+	
+	while(i <= 10)
+	{
+		car = 'a';
+
+		while(car <= 'z')
+		{
+			_putchar(car);
+				car++;
+		}
+			_putchar('\n');
+			i++;
+	}
+
+	return(0);
+}

c49ef85066d7bf1885e1215226ac5fe2b0caa004
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-07-06 13:35:21 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign- to print a sign of the number
+ * @n : number to check
+ * Return:1, 0 or -1
+ */
+int print_sign(int n)
+{
+	int numder;
+
+	if (n > 0)
+	{
+		number = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		number = 0;
+		_putchar('0');
+	}
+	else (n < 0)
+	{
+		number = -1;
+		_putchar('-');
+	}
+
+	return 0;
+}

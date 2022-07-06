2ef00bc4c48d0b3ce962d7beff553bc2c44ce116
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-07-05 17:12:00 -0700

    Task 0 _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -main block
+ * Description:print _putchar followed on a new line
+ * Return {0} Always
+ *
+ */
+int main(void )
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return 0;
+}

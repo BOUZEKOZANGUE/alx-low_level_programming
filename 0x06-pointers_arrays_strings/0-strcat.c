a78dffb91c44ee3d611c28be3cb1d468159f6d06
BOUZEKOZANGUE <adrienbouzeko@gmail.com>
2022-07-13 16:08:18 -0700

    Task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * strcat - the function that concatenate a string 
+ * @dest -to a character that will be changed
+ * @src -to a character that will be changed
+ * return: dest
+ */
+char *_strcat(char *dest, char *src)
+{	
+	int i,j;
+
+	i= 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j=0;
+	while (dest[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	
+	return (dest);
+}

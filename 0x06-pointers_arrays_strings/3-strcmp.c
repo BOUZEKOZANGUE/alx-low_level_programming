8f3e9e70ec109726f331be50e9fcbdd692c4365b
BOUZEKOZANGUE <107350957+BOUZEKOZANGUE@users.noreply.github.com>
2022-07-14 12:21:46 +0200

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
- * _strcmp - a function that compares two strings
- * @s1:The function of strings
- * @s2:The function of strings
- * return: Always 0.
+ *_strcmp - a function that compares two strings
+ *
+ * @s1: The function of strings
+ * @s2: The function of strcmp
+ *
+ * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,6 +15,7 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
+
 	if (*s1 == *s2)
 	{
 		return (0);

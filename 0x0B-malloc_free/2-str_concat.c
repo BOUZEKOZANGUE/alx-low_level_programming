53099a302242e32e77028434ba01d11c49f3a446
BOUZEKOZANGUE <107350957+BOUZEKOZANGUE@users.noreply.github.com>
2022-07-27 01:59:51 +0200

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- *  *str_concat - main function
- *  @s1: Primer string.
- *  @s2: Segundo string.
- *  Description:The returned pointer should point to a newly allocated space in memory which contains the contents
- *  Return: The pointer to str.
+ * *str_concat - main function.
+ * @s1: first string.
+ * @s2: Second string.
+ *
+ * Description: Write a function that returns a pointer to a newly allocated space in memory.
+ * a newly allocated space in memory, which contains
+ * a copy of the string given as a parameter.
+ * Return: The pointer to str.
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int size;/*the size of the int */
-	int size2;
-	int i, j;
-	char *array;
+	int size; /*  Size of the string*/
+	int size2; /*Size of second string */
+	int i, j; /* Loop Iterators */
+	char *array; /* Output  */
 
 	if (!s1)
-		s1 = "";
+		s1 = ""; /* Returns an empty string */
 	if (!s2)
 		s2 = "";
-	for (size = 0; s1[size] != '\0'; size++);
-	for (size2 = 0; s2[size2] != '\0'; size2++);
-	array = malloc(((size) + (size2 + 1)) * sizeof(char));/* the new allocation memory*/
+	for (size = 0; s1[size] != '\0'; size++) /* count string */
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++) /* count string */
+		;
+	array = malloc(((size) + (size2 + 1)) * sizeof(char)); /* New memory alloc */
 
 	if (!array)
 	{
-		return (NULL);
+		return (NULL); /* Returns NULLL if malloc fails */
 	}
 	for (i = 0; i < size; i++)
 	{
-		array[i] = s1[i];
+		array[i] = s1[i]; /
 	}
 	for (j = 0; j < (size2 + 1); j++)
 	{
-		array[i + j] = s2[j]; /* Co */
+		array[i + j] = s2[j]; 
 	}
 	return (array);
-
 }

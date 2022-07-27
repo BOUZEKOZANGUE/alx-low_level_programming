1015eeddd6fc3d443d043cec190aa04b591e74f3
BOUZEKOZANGUE <107350957+BOUZEKOZANGUE@users.noreply.github.com>
2022-07-27 02:20:02 +0200

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,32 +14,32 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size; /*  Size of the string*/
-	int size2; /*Size of second string */
-	int i, j; /* Loop Iterators */
-	char *array; /* Output  */
+	int size; /* Tamaño del string*/
+	int size2; /* Tamañano del segundo string */
+	int i, j; /* Iteradores de bucle */
+	char *array; /* Salida */
 
 	if (!s1)
-		s1 = ""; /* Returns an empty string */
+		s1 = ""; /* Devuelve un string vacio */
 	if (!s2)
 		s2 = "";
-	for (size = 0; s1[size] != '\0'; size++) /* count string */
+	for (size = 0; s1[size] != '\0'; size++) /* leng del string */
 		;
-	for (size2 = 0; s2[size2] != '\0'; size2++) /* count string */
+	for (size2 = 0; s2[size2] != '\0'; size2++) /* leng string */
 		;
 	array = malloc(((size) + (size2 + 1)) * sizeof(char)); /* New memory alloc */
 
 	if (!array)
 	{
-		return (NULL); /* Returns NULLL if malloc fails */
+		return (NULL); /* Devuelve NULLL si malloc falla */
 	}
 	for (i = 0; i < size; i++)
 	{
-		array[i] = s1[i]; /
+		array[i] = s1[i]; /* Copiamos string en base a string dado por main */
 	}
 	for (j = 0; j < (size2 + 1); j++)
 	{
-		array[i + j] = s2[j]; 
+		array[i + j] = s2[j]; /* Copiamos string en base a string dado por main */
 	}
-	return (array);
+	return (array); /* Devolvemos string nuevo */
 }

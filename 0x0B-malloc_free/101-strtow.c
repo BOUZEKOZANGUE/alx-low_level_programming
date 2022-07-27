cfe5aa137767b38cc128fcbf2ee70ebbefc115bd
BOUZEKOZANGUE <107350957+BOUZEKOZANGUE@users.noreply.github.com>
2022-07-27 03:02:39 +0200

    ok

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,76 +1,42 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * wrdcnt - counts the number of words in a string
- * @s: string to count
- *
- * Return: int of number of words
- */
-int wrdcnt(char *s)
+ * *argstostr - concatenates all arguements to the program
+ * @ac: arguement count
+ * @av: pointer to arguements
+ * Return: pointer to new space in memory or null
+ **/
+char *argstostr(int ac, char **av)
 {
-	int i, n = 0;
+	char *strDup;
+	int i, j, k, size;
 
-	for (i = 0; s[i]; i++)
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+/* count the number of chars in each string */
+	for (i = 0; i < ac; i++)
 	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
 	}
-	n++;
-	return (n);
-}
-
-/**
- * strtow -  splits a string into words
- *
- * @str: string to splear
- *
- * Return:  pointer to an array of strings (words)
+	size++;
+/* allocate memory for total number of chars and
+ * new line for each word
  */
-char **strtow(char *str)
-{
-	int i, j, k, l, n = 0, qa = 0;
-	char **q;
-
-	if (str == NULL || *str == '\0')
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
 		return (NULL);
-	n = wrdcnt(str);
-	if (n == 1)
-		return (NULL);
-	q = (char **)malloc(n * sizeof(char *));
-	if (q == NULL)
-	return (NULL);
-		q[n - 1] = NULL;
-		i = 0;
-	while (str[i])
+	k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-		for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-			;
-			j++;
-			q[qa] = (char *)malloc(j * sizeof(char));
-			j--;
-			if (q[qa] == NULL)
-			{
-				for (k = 0; k < qa; k++)
-					free(q[k]);
-				free(q[n - 1]);
-					free(q);
-				return (NULL);
-			}
-			for (l = 0; l < j; l++)
-				q[qa][l] = str[i + l];
-			q[qa][l] = '\0';
-				qa++;
-				i += j;
+			strDup[k++] = av[i][j];
 		}
-		else
-			i++;
+		strDup[k++] = '\n';
 	}
-	return (q);
+	strDup[k] = '\0';
+	return (strDup);
 }

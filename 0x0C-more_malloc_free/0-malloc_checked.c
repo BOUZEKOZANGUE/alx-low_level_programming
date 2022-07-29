ef36d0728c5b58cf9dedf1b3c8b2c6c6d7ded86a
BOUZEKOZANGUE <107350957+BOUZEKOZANGUE@users.noreply.github.com>
2022-07-29 22:12:00 +0200

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * *malloc_checked - check malloc
- * @b:size of memory
- * Description:allocates memory using malloc
- * Return:pointer to the allocated memory
+ * malloc_checked - allocate dynamic memory using malloc
+ * @b: the number of bytes to allocate
+ *
+ * Description: If memory allocation fails, cause normal process termination
+ * with a status value of 98.
+ *
+ * Return: Upon success, return a pointer to the newly allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
+void *p = malloc(b);
 
-	void *memory = 0;
-
-	memory = malloc(b);
-
-	if (!memory)
-		exit(98);
-
-	return (memory);
+if (p)
+return (p);
+exit(98);
 }
